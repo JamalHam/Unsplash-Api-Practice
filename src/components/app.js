3cf4fcf8b2e421204cac297e2868ce8925e8eb70
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-10-06 20:55:48 -0400

    images are loading

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash'
 import Searchbar from './SearchBar';
+import ImageList from './ImageList'
 
 
 class App extends React.Component {
     state = { images: [] };
 
     onSearchSubmit = async (term) =>{
-        const response = await axios
-            .get('https://api.unsplash.com/search/photos',{
+        const response = await unsplash.get('/search/photos',{
                 params:{ query: term },
-                headers: {
-                    Authorization: 'Client-ID fc5696cb8ef536c364e5b99e5c7ee32a89fa0e290b7d8eee6a68155dfd08993b'
-                }
             });
             this.setState({ images: response.data.results });
     }
@@ -21,7 +18,7 @@ render(){
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>    
                 <Searchbar onSubmit={this.onSearchSubmit}/>
-                Found: {this.state.images.length} images
+                <ImageList images={this.state.images}/>
             </div>
         );
     }
